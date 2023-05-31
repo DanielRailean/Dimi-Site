e75bbbb821859fabfcc46eb7389072a681d98761
littlelenka3 <lenka.orincakova@lego.com>
2023-06-01 00:44:27 +0200

    Test

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -4,16 +4,11 @@
 
 export const siteBaseUrl = 'https://www.linkedin.com/in/dimitrian-cebotaru-72968b200';
 
-export const keywords = [
-	'Dimitrian Cebotaru',
-	'Architect',
-	'VIA UC',
-	'VIA University college'
-];
+export const keywords = ['Dimitrian Cebotaru', 'Architect', 'VIA UC', 'VIA University college'];
 
 export const description =
-"I believe architecture should be great for everyone — those who write it and those who use it — and that bringing joy to problem-solving is the key to that."
+	'I believe architecture should be great for everyone — those who write it and those who use it — and that bringing joy to problem-solving is the key to that.';
 
 export const title = 'Dimitrian Cebotaru';
 
-export const image = `${siteBaseUrl}/images/site-preview.png`;
+export const image = `${siteBaseUrl}/images/site-preview.jpg`;
