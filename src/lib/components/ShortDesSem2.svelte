3e871883fece01cabee79775f39ce4e8bfcf45b0
Lenka Orincakova <lenka.orincakova@lego.com>
2023-06-19 23:26:13 +0200

    images

--- src/lib/components/ShortDesSem2.svelte
+++ src/lib/components/ShortDesSem2.svelte
@@ -3,16 +3,25 @@
 	import { entries } from '$lib/data/experience';
 	import { sem2 } from '$lib/semesters';
 	import ProjectsIcon from '$lib/icons/projects.svelte';
+	import Picture2 from '$lib/images/2nd.png?width=400&format=avif;webp;png&meta&imagetools';
+	import { getSrcsetFromImport } from '$lib/utils/functions';
+
 	//     import Tab1 from "./Tab1.svelte";
 	// 	import Tab2 from "./Tab2.svelte";
 	// 	import Tab3 from "./Tab3.svelte";
 	//   import Tabs from "./Tabs.svelte";
+	const photo2 = getSrcsetFromImport(Picture2);
 </script>
 
-<div class="main">
-	The project methodology in the second semester is applied to the following themes: Industry and
-	Technology, International Project Week and Designing a Two-storey Single-family House with a
-	Basement.
+<div class="desc-photo" style="display:flex">
+	<div class="main">
+		The project methodology in the second semester is applied to the following themes: Industry and
+		Technology, International Project Week and Designing a Two-storey Single-family House with a
+		Basement.
+	</div>
+	<div class="photo" style="margin-top: -6rem; padding-left: 2rem; ">
+		<img style="border-radius:25px" src={photo2.png} alt="Dimitrian" />
+	</div>
 </div>
 
 <style lang="scss">
@@ -31,12 +40,13 @@
 
 	.main {
 		display: flex;
+		width: 80%;
 		flex-direction: row;
 		justify-content: center;
 		padding-left: 30%;
 		font-size: 0.8rem;
 		padding-bottom: 3%;
-		padding-right: 30%;
+		// padding-right: 30%;
 		color: black;
 		font-family: 'Cera Pro';
 	}
--- src/lib/components/Shortsem3.svelte
+++ src/lib/components/Shortsem3.svelte
@@ -2,16 +2,28 @@
 	import Button from '$lib/components/atoms/Button.svelte';
 	import { entries } from '$lib/data/experience';
 	import { sem2 } from '$lib/semesters';
+	import Picture3 from '$lib/images/3rd.png?width=300&format=avif;webp;png&meta&imagetools';
+	import { getSrcsetFromImport } from '$lib/utils/functions';
+	const photo3 = getSrcsetFromImport(Picture3);
 </script>
 
-<div class="main">
-	The overall theme in the 3rd semester is "Industrialised building design". As with the previous
-	two semesters, the predominant work method was project work - carried out as group work whenever
-	possible.
+<div class="desc-photo" style="display:flex">
+	<div class="main">
+		The overall theme in the 3rd semester is "Industrialised building design". As with the previous
+		two semesters, the predominant work method was project work - carried out as group work whenever
+		possible.
+	</div>
+	<div class="photo" style="margin-top: -6rem; padding-left: 2rem; ">
+		<img class="image" src={photo3.png} alt="Dimitrian" />
+	</div>
 </div>
 
 <style lang="scss">
 	@import '$lib/scss/breakpoints.scss';
+	.image {
+		border-radius: 25px;
+		height: 12.5rem;
+	}
 	.outline {
 		// display: flex;
 		padding: 5px 15px;
@@ -25,12 +37,13 @@
 
 	.main {
 		display: flex;
+		width: 80%;
 		flex-direction: row;
 		justify-content: center;
 		padding-left: 30%;
 		font-size: 0.8rem;
 		padding-bottom: 3%;
-		padding-right: 30%;
+		// padding-right: 30%;
 		color: black;
 		font-family: 'Cera Pro';
 	}
